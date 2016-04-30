462e206ca8af1786116ffbeb6e47cdec792c1c96
JordanCarr <Jordan.Carr@me.com>
2016-04-29 21:35:50 -0600

    fixed submission version

--- src/bigBucksRevisited/BankAccount.java
+++ src/bigBucksRevisited/BankAccount.java
@@ -14,16 +14,13 @@ class BankAccount {
         balance = amt;
     }
 
-// --Commented out by Inspection START (29/04/16 4:11 PM):
-//    public void deposit(double dp) {
-//        balance = balance + dp;
-//    }
-// --Commented out by Inspection STOP (29/04/16 4:11 PM)
+    public void deposit(double dp) {
+        balance = balance + dp;
+    }
 
-// --Commented out by Inspection START (29/04/16 4:11 PM):
-//    public void withdraw(double wd) {
-//        balance = balance - wd;
-//    }
-// --Commented out by Inspection STOP (29/04/16 4:11 PM)
+    public void withdraw(double wd) {
+        balance = balance - wd;
+    }
+
 }
 
